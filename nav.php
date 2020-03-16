af6e8e8b4c92150ff71b47c962334eb142727c2e
manuel012414 <33933922+manuel012414@users.noreply.github.com>
2020-03-16 16:59:28 +0100

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,6 @@
+<nav>
+    <a href="contacte.php" target="_blank">Link-1/</a>
+    <a href="altres.php" target="_blank">Link-2/</a>
+    <a href="https://www.juegosjuegos.ws/" target="_blank">Link-3/</a>
+    <a href="https://es.wikipedia.org/wiki/Pakist%C3%A1n" target="_blank">Link-4</a>
+</nav>
